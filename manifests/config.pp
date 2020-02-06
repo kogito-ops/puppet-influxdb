3c3af96e9ab890592476217d05f7b809aa2c73c1
Woth-Ursula <uwoth@kogito.network>
2020-02-06 10:23:13 +0100

    chmod and name of params

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,9 +9,9 @@ class influxdb::config (
   String $configuration_file = $::influxdb::params::configuration_file,
   String $service_defaults = $::influxdb::params::service_defaults,
   String $service_definition = $::influxdb::params::service_definition,
-  String $directory_metadataraft = $::influxdb::params::directory_metadataraft,
-  String $directory_tsmdata = $::influxdb::params::directory_tsmdata,
-  String $directory_tsmwal = $::influxdb::params::directory_tsmwal,
+  String $metadata_raft = $::influxdb::params::directory_metadataraft,
+  String $tsm_data = $::influxdb::params::directory_tsmdata,
+  String $tsm_wal = $::influxdb::params::directory_tsmwal,
   String $user = $::influxdb::params::user,
   String $group = $::influxdb::params::group,
   String $package = $::influxdb::params::package,
@@ -23,12 +23,14 @@ class influxdb::config (
     ensure => 'directory',
     owner  => $::influxdb::params::user,
     group  => $::influxdb::params::group,
+    mode   => '0766',
   }
 
   -> file { "${::influxdb::params::configuration_path}/${::influxdb::params::configuration_file}":
     ensure  => 'present',
     owner   => $::influxdb::params::user,
     group   => $::influxdb::params::group,
+    mode    => '0644',
     content => template('influxdb/influxdb.conf.erb')
   }
 
@@ -36,6 +38,7 @@ class influxdb::config (
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
+    mode    => '0644',
     content => template('influxdb/service-defaults.erb'),
   }
 
@@ -43,24 +46,28 @@ class influxdb::config (
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
+    mode    => '0644',
     content => template('influxdb/systemd.service.erb'),
   }
 
-  -> file { $::influxdb::params::directory_tsmdata:
+  -> file { $::influxdb::params::tsm_data:
     ensure => 'directory',
     owner  => $::influxdb::params::user,
     group  => $::influxdb::params::group,
+    mode   => '0755',
   }
 
-  -> file { $::influxdb::params::directory_tsmwal:
+  -> file { $::influxdb::params::tsm_wal:
     ensure => 'directory',
     owner  => $::influxdb::params::user,
     group  => $::influxdb::params::group,
+    mode   => '0755',
   }
 
-  -> file { $::influxdb::params::directory_metadataraft:
+  -> file { $::influxdb::params::metadata_raft:
     ensure => 'directory',
     owner  => $::influxdb::params::user,
     group  => $::influxdb::params::group,
+    mode   => '0755',
   }
 }
