1aa38a64e0ee0b9b9fc3659a4a54e6a197d0257c
Woth-Ursula <uwoth@kogito.network>
2020-02-06 09:27:41 +0100

    Changed to use params for user/group

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,21 +7,21 @@
 class influxdb::config {
   file { $::influxdb::params::configuration_path:
     ensure => 'directory',
-    owner  => 'influxdb',
-    group  => 'influxdb',
+    owner  => $::influxdb::params::user,
+    group  => $::influxdb::params::group,
   }
 
   -> file { "${::influxdb::params::configuration_path}/${::influxdb::params::configuration_file}":
     ensure  => 'present',
-    owner   => 'influxdb',
-    group   => 'influxdb',
+    owner   => $::influxdb::params::user,
+    group   => $::influxdb::params::group,
     content => template('influxdb/influxdb.conf.erb')
   }
 
   -> file { $::influxdb::params::service_defaults:
     ensure  => 'present',
-    owner   => 'influxdb',
-    group   => 'influxdb',
+    owner   => 'root',
+    group   => 'root',
     content => template('influxdb/service-defaults.erb'),
   }
 
@@ -34,19 +34,19 @@ class influxdb::config {
 
   -> file { $::influxdb::params::directory_tsmdata:
     ensure => 'directory',
-    owner  => 'influxdb',
-    group  => 'influxdb',
+    owner  => $::influxdb::params::user,
+    group  => $::influxdb::params::group,
   }
 
   -> file { $::influxdb::params::directory_tsmwal:
     ensure => 'directory',
-    owner  => 'influxdb',
-    group  => 'influxdb',
+    owner  => $::influxdb::params::user,
+    group  => $::influxdb::params::group,
   }
 
   -> file { $::influxdb::params::directory_metadataraft:
     ensure => 'directory',
-    owner  => 'influxdb',
-    group  => 'influxdb',
+    owner  => $::influxdb::params::user,
+    group  => $::influxdb::params::group,
   }
 }
