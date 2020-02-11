34e51a3d566d6368b062be54a354a779609281ee
Woth-Ursula <uwoth@kogito.network>
2020-02-11 12:40:18 +0100

    params

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,10 +5,10 @@
 # @example
 #   include influxdb::params
 class influxdb::params (
-  String $package_manager,
-  String $keyid,
-  String $keyid_source,
-  String $keyid_server,
+  String $package_manager = 'undef',
+  String $keyid = '05CE15085FC09D18E99EFB22684A14CFE0C5',
+  String $keyid_source = 'https://repos.influxdata.com/influxdb.key',
+  String $keyid_server ='eu.pool.sks-keyservers.net',
   String $configuration_path = '/etc/influxdb',
   String $configuration_file = 'influxdb.conf',
   String $service_defaults = '/etc/default/influxdb',
