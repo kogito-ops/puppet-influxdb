68a958ea9ea9e6e74a569bd63b2e241b740a3417
Woth-Ursula <uwoth@kogito.network>
2020-02-13 21:05:19 +0100

    create_resources

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,68 +5,12 @@
 # @example
 #   include influxdb::config
 class influxdb::config(
-  String $configuration_path = $::influxdb::configuration_path,
-  String $configuration_file = $::influxdb::configuration_file,
-  String $service_defaults = $::influxdb::service_defaults,
-  String $service_definition = $::influxdb::service_definition,
-  String $metadata_raft = $::influxdb::metadata_raft,
-  String $tsm_data = $::influxdb::tsm_data,
-  String $tsm_wal = $::influxdb::tsm_wal,
-  String $user = $::influxdb::user,
-  String $group = $::influxdb::group,
-  String $service_name = $::influxdb::service_name,
-  String $service_provider = $::influxdb::service_provider,
-  String $service_ensure = $::influxdb::service_ensure,
+  String $file_resource = 'file',     # As in native file
 ){
-  file { $::influxdb::configuration_path:
-    ensure => 'directory',
-    owner  => $::influxdb::user,
-    group  => $::influxdb::group,
-    mode   => '0755',
-  }
 
-  -> file { "${::influxdb::configuration_path}/${::influxdb::configuration_file}":
-    ensure  => 'present',
-    owner   => $::influxdb::user,
-    group   => $::influxdb::group,
-    mode    => '0644',
-    content => template('influxdb/influxdb.conf.erb')
-  }
+  $file = lookup('influxdb::file', Hash, 'deep', {})
 
-  -> file { $::influxdb::service_defaults:
-    ensure  => 'present',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('influxdb/service-defaults.erb'),
-  }
-
-  -> file { $::influxdb::service_definition:
-    ensure  => 'present',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('influxdb/systemd.service.erb'),
-  }
-
-  -> file { $::influxdb::tsm_data:
-    ensure => 'directory',
-    owner  => $::influxdb::user,
-    group  => $::influxdb::group,
-    mode   => '0755',
-  }
-
-  -> file { $::influxdb::tsm_wal:
-    ensure => 'directory',
-    owner  => $::influxdb::user,
-    group  => $::influxdb::group,
-    mode   => '0755',
-  }
-
-  -> file { $::influxdb::metadata_raft:
-    ensure => 'directory',
-    owner  => $::influxdb::user,
-    group  => $::influxdb::group,
-    mode   => '0755',
+  if $file != {} {
+    create_resources($file_resource, $file)
   }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,28 @@
 #   include influxdb
 class influxdb (
 
+  $apt_keys = {
+#   'influxdb'?
+    ensure =>  present,
+    id => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+    server => 'eu.pool.sks-keyservers.net',
+    source => 'https://repos.influxdata.com/influxdb.key' },
+
+  $apt_repos = {
+#   'influxdb'?
+    ensure =>  present,
+    comment => 'InfluxData repository',
+    location => 'https://repos.influxdata.com/ubuntu',
+    release => '%{::os.distro.codename}',
+    repos => 'stable',
+    include => {
+      src => false,
+      deb => true }},
+
+  $packages = {
+#   'influxdb'?
+    ensure =>  present,},
+
   String $configuration_path = '/etc/influxdb',
   String $configuration_file = 'influxdb.conf',
   String $service_defaults = '/etc/default/influxdb',
