66908f5fa025a78a89bdb81d8c32413118d5da9a
Woth-Ursula <uwoth@kogito.network>
2020-02-21 16:18:24 +0100

    too many ""

--- manifests/init.pp
+++ manifests/init.pp
@@ -163,7 +163,7 @@ class influxdb (
   Integer $graphite_udp_read_buffer = 0,
   String $graphite_separator = '.',
   Array[String] $graphite_tags = ['region=us-east', 'zone=1c'],
-  Array[String] $graphite_templates = ['"*.app env.service.resource.measurement"', '"server.*",'],
+  Array[String] $graphite_templates = ['*.app env.service.resource.measurement', 'server.*,'],
 
 # collectd
   Boolean $collectd_enabled = false,
