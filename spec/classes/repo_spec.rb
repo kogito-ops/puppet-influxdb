2bf7f49940b9ca23544ceb788518d80bf53f911d
Woth-Ursula <uwoth@kogito.network>
2020-03-03 12:48:18 +0100

    spec repo

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -2,49 +2,15 @@ require 'spec_helper'
 
 describe 'influxdb::repo', type: :class do
   on_supported_os.each do |facts|
-    context 'with :name => "Ubuntu"' do
-      if facts[:name] == 'Ubuntu'
-        let :params do
-          {
-            keys => lookup('influxdb::gpg_keys', Hash, 'deep', {}),
-            key_resource: 'apt::key',
-            resource: 'apt::source',
-            repositories => lookup('influxdb::repositories', Hash, 'deep', {}),
-          }
-        end
-
-        it { is_expected.to compile }
+    context 'with all defaults' do
+      let :params do
+        {
+          key_resource: '',
+          resource: '',
+        }
       end
-    end
 
-    context 'with :name => "Debian"' do
-      if facts[:name] == 'Debian'
-        let :params do
-          {
-            keys => lookup('influxdb::gpg_keys', Hash, 'deep', {}),
-            key_resource: 'apt::key',
-            resource: 'apt::source',
-            repositories => lookup('influxdb::repositories', Hash, 'deep', {}),
-          }
-        end
-
-        it { is_expected.to compile }
-      end
-    end
-
-    context 'with :name => "CentOS"' do
-      if facts[:name] == 'CentOS'
-        let :params do
-          {
-            keys: '',
-            key_resource: '',
-            resource: 'yumrepo',
-            repositories => lookup('influxdb::repositories', Hash, 'deep', {}),
-          }
-        end
-
-        it { is_expected.to compile }
-      end
+      it { is_expected.to compile }
     end
   end
 end
