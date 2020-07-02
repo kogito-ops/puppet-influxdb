4fdc6e9437310cd449fa41c228fe37f27143b8f5
Woth-Ursula <uwoth@kogito.network>
2020-07-02 12:10:52 +0200

    fixed rubocop offenses
    Signed-off-by: Woth-Ursula <uwoth@kogito.network>

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -20,14 +20,6 @@ describe 'influxdb::repo', type: :class do
           is_expected.to compile.with_all_deps
           is_expected.to contain_package('influxdb')
           is_expected.to contain_class('apt')
-#          is_expected.to contain_apt__source('influxdata').with(
-#            location: 'https://repos.influxdata.com/',
-#            repos: 'stable',
-#            release: facts[:lsbdistcodename],
-#            key: {
-#              'id' => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-#              'source' => 'https://repos.influxdata.com/influxdb.key'
-#            })
         end
 
         if facts[:osname] == 'CentOS'
