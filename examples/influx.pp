3d978de74eff72847d9a277f7a29965b5253cddb
Woth-Ursula <uwoth@kogito.network>
2020-08-24 15:20:22 +0200

    Changed examples
    Signed-off-by: Woth-Ursula <uwoth@kogito.network>

--- examples/influx.pp
+++ examples/influx.pp
@@ -1,5 +1,25 @@
 include ::influxdb
 
+# is there another influxdata module, which handles already the gpg key and the repo
+class { 'influxdb':
+  manage_repo => false,
+}
+
+# influxdb handels the gpg key and the repo
 class { 'influxdb':
   manage_repo => true,
+}
+
+# define a database
+influxdb::database { 'telegraf1':
+  ensure => present,
+}
+# and a user
+influxdb::user { 'telegraf1':
+  password => 'metricsmetricsmetrics',
+}
+# with proper access
+influxdb::grant { 'telegraf1':
+  grant    => 'WRITE',
+  database => 'telegraf',
 }
